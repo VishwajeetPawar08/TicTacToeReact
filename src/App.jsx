6efb3c58547d2d858ec3f9da51eaac7451105d67
Vishwajeet Pawar <vishwajeet.pawarrr@gmail.com>
2024-02-06 19:00:53 +0530

    useState added to change counter and board commented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react';
+
 import './components/styles.scss';
-import Board from './components/Board';
+// import Board from './components/Board';
 
 function App() {
 
+  const [counter, setCounter] = useState(1);
+
+  const runn = ()=>{
+    setCounter(counter+1);
+  }
+
   return (
+
     <div className='app'>
-      <Board></Board>
+
+      <div>
+        <button onClick={runn}>{counter}</button>
+      </div>
+      {/* <Board></Board> */}
     </div>
   )
 }
