4941f07aae5be3687922e1776f7385245ca436b8
Vishwajeet Pawar <vishwajeet.pawarrr@gmail.com>
2024-02-08 16:08:12 +0530

    added history and styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,42 +6,72 @@ import { useState } from 'react';
 import './components/styles.scss';
 import Board from './components/Board.jsx';
 import StatusMessage from './components/StatusMessage.jsx';
+import History from './components/History.jsx';
 import { calculateWinner } from './winner.js';
 
 function App() {
   // const [counter, setCounter] = useState(1);
-
   // const runn = ()=>{
   //   setCounter(counter+1);
   // }
 
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
+  // const [squares, setSquares] = useState(Array(9).fill(null));
+  // const [isXNext, setIsXNext] = useState(false);
 
-  const winner = calculateWinner(squares);
- 
+  const [history, setHistory] = useState([
+    { squares: Array(9).fill(null), isXNext: false },
+  ]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const gamingBoard = history[currentMove];
+
+  const winner = calculateWinner(gamingBoard.squares);
+
   const onClickSquare = clickedPosition => {
-    if (squares[clickedPosition] || winner) {
+    if (gamingBoard.squares[clickedPosition] || winner) {
       return;
     }
 
-    setSquares(currentSquares => {
-      return currentSquares.map((squareValue, position) => {
-        if (clickedPosition === position) {
-          return isXNext ? 'X' : 'O';
-        }
+    setHistory(currentHistory => {
+      const isTraversing = currentMove + 1 !== currentMove.length;
 
-        return squareValue;
+      const lastGamingState = isTraversing
+        ? currentHistory[currentMove]
+        : history[history.length - 1];
+
+      const nextSquaresState = lastGamingState.squares.map(
+        (squareValue, position) => {
+          if (clickedPosition === position) {
+            return lastGamingState.isXNext ? 'X' : 'O';
+          }
+
+          return squareValue;
+        }
+      );
+
+      const base = isTraversing
+        ? currentHistory.slice(0, currentHistory.indexOf(lastGamingState) + 1)
+        : currentHistory;
+
+      return base.concat({
+        squares: nextSquaresState,
+        isXNext: !lastGamingState.isXNext,
       });
     });
 
-    setIsXNext(currentIsXNext => !currentIsXNext);
+    // setIsXNext(currentIsXNext => !currentIsXNext);
+    setCurrentMove(move => move + 1);
+  };
+
+  const moveTo = move => {
+    setCurrentMove(move);
   };
 
   return (
     <div className="app">
-      <StatusMessage winner={winner} isXNext = {isXNext} squares={squares} />
-      <Board squares={squares} onClickSquare={onClickSquare} />
+      <StatusMessage winner={winner} gamingBoard={gamingBoard} />
+      <Board squares={gamingBoard.squares} onClickSquare={onClickSquare} />
+
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 }
--- src/components/StatusMessage.jsx
+++ src/components/StatusMessage.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-const StatusMessage = ({ winner, isXNext, squares }) => {
+const StatusMessage = ({ winner, gamingBoard }) => {
+  const { squares, isXNext } = gamingBoard;
+
   const noMovesLeft = squares.every(squareValue => squareValue !== null);
   const nextPlayer = isXNext ? 'X' : 'O';
 
@@ -14,16 +16,24 @@ const StatusMessage = ({ winner, isXNext, squares }) => {
     }
     if (!winner && !noMovesLeft) {
       return (
-        <> 
-            {/* react fragment */}
-          <h3>Next chance is to <span className={isXNext? "text-orange":"text-green"}>{nextPlayer}</span></h3>
+        <>
+          {/* react fragment */}
+          <h3>
+            Next chance is to{' '}
+            <span className={isXNext ? 'text-orange' : 'text-green'}>
+              {nextPlayer}
+            </span>
+          </h3>
         </>
       );
     }
     if (!winner && noMovesLeft) {
       return (
         <div>
-          <h3>No moves left. <span className="text-orange">O</span> and <span className="text-green">X</span> TIED...</h3>
+          <h3>
+            No moves left. <span className="text-orange">O</span> and{' '}
+            <span className="text-green">X</span> TIED...
+          </h3>
         </div>
       );
     }
